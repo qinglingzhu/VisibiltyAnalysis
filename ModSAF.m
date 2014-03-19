8febadba5b7eab6975c5f69ab3d7fe728a60054e
qinglingzhu <qlzhu1991@gmail.com>
2014-03-19 13:09:30 +0800

    janus and modsaf ect.
    begin to write the algorithm :janus and modsaf ect.

---
+++ ModSAF.m
@@ -0,0 +1,8 @@
+%功能：添加西北到东南方向的对角线。根据点所在的三角面来求得该点的高程值
+%输入：基于网格的高程数据
+%输出：某个断面的数据
+%作者：Qingling.Zhu emial:qlzhu1991@gmail.com
+%时间：2014-3-19
+%reference to ：Modular semi-automated forces (Andy Ceranowicz)
+function ModSAF(DEM_X,DEM_Y,DEM_Z)
+end
